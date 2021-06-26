0721d17007db2b1c48164c92fdfda994655ee04a
SantiagoSala <aa2793@student.jamk.fi>
2021-06-26 23:04:23 +0300

    change code from classes to hooks

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -1,4 +1,4 @@
-import React, { Componen, Component } from 'react';
+import React, { Component } from 'react';
 
 class ErrorBoundry extends Component {
   constructor(props) {
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
@@ -6,51 +6,55 @@ import ErrorBoundry from '../components/ErrorBoundary';
 
 import './App.css';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: [],
-      searchfield: '',
-    };
-  }
+function App() {
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     robots: [],
+  //     searchfield: '',
+  //   };
+  // }
+  const [robots, setRobots] = useState([]);
+  const [searchField, setSearchField] = useState('');
 
-  componentDidMount() {
+  // componentDidMount() {
+  //   fetch('https://jsonplaceholder.cypress.io/users')
+  //     .then((response) => response.json())
+  //     .then((users) => {
+  //       this.setState({ robots: users });
+  //     });
+  // }
+
+  useEffect(() => {
     fetch('https://jsonplaceholder.cypress.io/users')
       .then((response) => response.json())
       .then((users) => {
-        this.setState({ robots: users });
+        setRobots(users);
       });
-  }
+  }, []);
 
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value });
+  const onSearchChange = (event) => {
+    setSearchField(event.target.value);
   };
+  const filteredRobots = robots.filter((robot) => {
+    return robot.name
+      .toLocaleLowerCase()
+      .includes(searchField.toLocaleLowerCase());
+  });
 
-  render() {
-    const { robots, searchfield } = this.state;
-
-    const filteredRobots = robots.filter((robot) => {
-      return robot.name
-        .toLocaleLowerCase()
-        .includes(searchfield.toLocaleLowerCase());
-    });
-    if (!robots.length === 0) {
-      return <h1>Loading</h1>;
-    } else {
-      return (
-        <div className="tc">
-          <h1 className="f1">My Robofriends</h1>
-          <SearchBox searchChange={this.onSearchChange} />
-          <Scroll>
-            <ErrorBoundry>
-              <CardList robots={filteredRobots} />;
-            </ErrorBoundry>
-          </Scroll>
-        </div>
-      );
-    }
-  }
+  return !robots.length ? (
+    <h1>Loading</h1>
+  ) : (
+    <div className="tc">
+      <h1 className="f1">My Robofriends</h1>
+      <SearchBox searchChange={onSearchChange} />
+      <Scroll>
+        <ErrorBoundry>
+          <CardList robots={filteredRobots} />;
+        </ErrorBoundry>
+      </Scroll>
+    </div>
+  );
 }
 
 export default App;
